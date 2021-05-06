a20a014ac7cc549c9ec37c8336c15d32e977b9cf
rishsoti <62929356+rishsoti@users.noreply.github.com>
2021-05-06 23:16:45 +0530

    Update CowinScheduler.py

--- CowinScheduler.py
+++ CowinScheduler.py
@@ -4,27 +4,26 @@ Created on Thu May  6 20:54:03 2021
 
 @author: rishabh.soti
 """
-
+import time
 import numpy as np
 import pandas as pd
 import smtplib
 from cowin_api import CoWinAPI
 
+date = '09-05-2021'
+min_age_limit = 18 
+empty_array = np.empty((0,6), int)
 
-pin_code = "243001"
-date = '09-05-2021'  # Optional. Default value is today's date
-min_age_limit = 45  # Optional. By default returns centers without filtering by min_age_limit 
-
-
-empty_array = np.empty((0, 4), int)
 cowin = CoWinAPI()
-available_centers = cowin.get_availability_by_pincode(pin_code, date, min_age_limit)
-print(available_centers)
-for i in available_centers['centers']:
-   for x in i['sessions']:
-        empty_array = np.append(empty_array, np.array([[str(i['name']),str(i['address']), str(x['available_capacity']), str(x['vaccine']), str(x['date'])]]), axis=0)
+districts = cowin.get_districts(34)
+for i in districts['districts']:
+    available_centers = cowin.get_availability_by_district(i['district_id'], date, min_age_limit)
+    for i in available_centers['centers']:
+       for x in i['sessions']:
+           if int(x['available_capacity'])>0 :
+             empty_array = np.append(empty_array, np.array([[str(i['name']),str(i['address']), str(x['available_capacity']), str(x['vaccine']), str(x['date']), str(x['slots'])]]), axis=0)
        
-headers = ['Name', 'Avilability', 'Vaccine', 'Date']
+headers = ['Name','Address', 'Avilability', 'Vaccine', 'Date','Slots']
 df = pd.DataFrame(empty_array, columns=headers)
 html = df.to_html()
          
@@ -35,7 +34,7 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
   
 # Authentication
-s.login("no.reply.alert0@gmail.com", "FLYhigh91!")
+s.login("no.reply.alert0@gmail.com", "noreplyuser")
   
 # message to be sent
 message = """From:  <from@fromdomain.com>
@@ -44,16 +43,14 @@ MIME-Version: 1.0
 Content-type: text/html
 Subject: Alert!! Vaccine Slot Avilable
 
-
-
-
 <h2><b> Book Your Appointment!!</b></h2>
 <h2> </h2>
 <h3> In any of the given hospital from Cowin App</h2>
+<h3> -------------------------------------------</h2>
 """ +html
-  
+time.sleep(2.4)
 # sending the mail
-s.sendmail("no.reply.alert0@gmail.com", "vs230135@gmail.com", message)
-  
+s.sendmail("no.reply.alert0@gmail.com", "rishabhsoti16@gmail.com", message)
+time.sleep(2.4)
 # terminating the session
 s.quit()
